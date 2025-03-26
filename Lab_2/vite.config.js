ccb043b07a2adf29818c9a28c10c8a6e889308e8
MykhailoMatsyshyn <matsyshynmykhailo@gmail.com>
2025-03-26 06:58:11 +0200

    update vite conf

--- Lab_2/vite.config.js
+++ Lab_2/vite.config.js
@@ -12,4 +12,15 @@ export default {
       certDir: "/Users/.../.devServer/cert",
     }),
   ],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        task1: resolve(__dirname, "src/task1.html"),
+        task2: resolve(__dirname, "src/task2.html"),
+        task3: resolve(__dirname, "src/task3.html"),
+        task4: resolve(__dirname, "src/task4.html"),
+      },
+    },
+  },
 };
